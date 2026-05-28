cec0158f72997bd1c60ed6cbbc728c5f8578f60d
coffeeaddictamoeba <vika.pokalo.2023@gmail.com>
2026-05-28 16:21:10 +0300

    init the cpp code structure

---
+++ Raspberry/include/defaults.h
@@ -0,0 +1,4 @@
+
+#define MODEL_DEFAULT "rock_paper_scissors_model_f32.tflite"
+#define PREDICTION_FILE_DEFAULT "test.png"
+#define RECORDING_DATA_DEFAULT "rock_paper_scissors_data_recorded.csv"
---
+++ Raspberry/src/main.cpp
@@ -0,0 +1,52 @@
+#include <cstdio>
+#include <cstring>
+#include <string>
+
+#include "../include/defaults.h"
+
+struct config {
+    std::string model     = MODEL_DEFAULT;
+    std::string pred_file = PREDICTION_FILE_DEFAULT;
+    std::string rec_file  = RECORDING_DATA_DEFAULT;
+    bool is_on = false;
+    bool verbose = false;
+};
+
+int parse_args(int argc, char** argv, config& cf) {
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--start") == 0) {
+            cf.is_on = true;
+        } else if (strcmp(argv[i], "--predict") == 0 && i+1 < argc) {
+            cf.pred_file = argv[++i];
+        } else if (strcmp(argv[i], "--model") == 0 && i+1 < argc) {
+            cf.model = argv[++i];
+        } else if (strcmp(argv[i], "--record-to") == 0 && i+1 < argc) {
+            cf.rec_file = argv[++i];
+        } else if (strcmp(argv[i], "--verbose") == 0) {
+            cf.verbose = true;
+        } else {
+            fprintf(
+                stderr, 
+                "[ERROR] Unknown argument: %s\n", 
+                argv[i]
+            );
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    config cf; 
+    parse_args(argc, argv, cf);
+
+    if (cf.verbose) {
+        printf(
+            "[INFO] Running rock-paper-scissors with:\n\t"
+            " Model:  %s\n\t "
+            " Predict: %s\n\t "
+            " Record: %s\n",
+            cf.model.c_str(), 
+            cf.pred_file.c_str(), 
+            cf.rec_file.c_str()
+        );
+    }
+}
